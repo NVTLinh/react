acb9ec6cb6b76015351b04c88227c6861553ae7e
NVTLinh <ducnv2007@gmail.com>
2024-07-06 21:05:37 +0700

    New

--- src/component/todo.jsx
+++ src/component/todo.jsx
@@ -3,6 +3,7 @@ export default function Todo() {
         <span>08:04 06/07/2024</span>
         <div>
             <button><i class="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
+            <button><i class="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
         </div>
         <span>Làm bài tập về nhà</span>
     </div>
