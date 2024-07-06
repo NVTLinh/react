9db4393ef6f90c602109a2135f3cfcff4d24c43c
NVTLinh <ducnv2007@gmail.com>
2024-07-06 21:30:11 +0700

    new

--- src/component/todo.jsx
+++ src/component/todo.jsx
@@ -2,8 +2,8 @@ export default function Todo() {
     <div>
         <span>08:04 06/07/2024</span>
         <div>
-            <button><i class="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
-            <button><i class="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
+            <button><i className="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
+            <button><i className="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
         </div>
         <span>Làm bài tập về nhà</span>
     </div>
--- src/pages/TudoList.jsx
+++ src/pages/TudoList.jsx
@@ -1,15 +1,46 @@
-
+import React, {useState} from "react";
+import Todo from "../component/todo";
 
 export default function TodoList() {
+
+    const [listTodo, setlistTodo] = useState([]);
+    const [todoContent, settodoContent] = useState('');
+
+    const listNewTodo = listTodo.filter(item => item.status_id === 1)
+    const listFinishedTodo = listTodo.filter(item => item.status_id === 2)
+    const listCanelledTodo = listTodo.filter(item => item.status_id === 3)
+
+    const handleSaveTodoContent = (event)=>{
+        settodoContent(event.target.value);
+    }
+
+    const handleCreaterTodo = (event) =>{
+        event.preventDefault();
+        if(!todoContent.trim()){
+            alert('Vui lòng nhập kế hoạch của bạn')
+            return
+        }
+    }
+
+    const obj = {
+        id: new Date().getTime(),
+        content: todoContent,
+        status_id: 1,
+        created_at: new Date().getTime(),
+    }
+    settodoContent('')
+    setlistTodo([...listTodo, obj])
+
     return (
         <div>
-            <form className="d-flex" role="search">
+            <form className="d-flex" role="search" onSubmit={handleCreaterTodo}>
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                <button className="btn btn-outline-success" type="submit">Lưu</button>
             </form>
             <div className="d-plex row mt-3 mb-3">
                 <div className="col-4">
                     <h3 className="text-xl bg-primary text-white rounded text-center py-2">Mới</h3>
+                    <Todo/>
                 </div>
                 <div className="col-4">
                     <h3 className="text-xl bg-success text-white rounded text-center py-2">Đã hoàn thành</h3>
