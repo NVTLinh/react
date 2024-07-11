780f0ae0f440c9e19d74921ac805dec944c767aa
NVTLinh <ducnv2007@gmail.com>
2024-07-11 20:31:56 +0700

    Hoàn thiện TodoList

--- src/component/todo.jsx
+++ src/component/todo.jsx
@@ -1,10 +1,28 @@
-export default function Todo() {
-    <div>
-        <span>08:04 06/07/2024</span>
-        <div>
-            <button><i className="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
-            <button><i className="fa-regular fa-trash-can" style="color: #ff0000;"></i></button>
+export default function Todo(props) {
+    const { todo, handleFinish, handleDelete, handleNew } = props;
+    return <div>
+        <div className="d-flex justify-content-between align-items-center">
+            <span>08:04 06/07/2024</span>
+            <div className="d-flex justify-content-between align-items-center">
+                {
+                    todo.status_id === 1 ?
+                        <button onClick={()=>{handleFinish(todo.id)}}><i className="fa-regular fa-trash-can" ></i>HT</button>
+                        : null
+                }
+                {
+                    todo.status_id !== 3 ?
+                        <button onClick={()=>{handleDelete(todo.id)}}><i className="fa-regular fa-trash-can" ></i>HỦY</button>
+                        : null
+                }
+                {
+                    todo.status_id === 3 ?
+                        <button onClick={()=>{handleNew(todo.id)}}><i className="fa-regular fa-trash-can"></i>Làm mới</button>
+                        : null
+                }
+            </div>
         </div>
-        <span>Làm bài tập về nhà</span>
+        <p className="fs-3 mb-0">
+            {todo.content}
+        </p>
     </div>
 }
--- src/pages/TudoList.jsx
+++ src/pages/TudoList.jsx
@@ -1,52 +1,99 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Todo from "../component/todo";
 
 export default function TodoList() {
+    const [listTodo, setListTodo] = useState([]);
+    const [todoContent, setTodoContent] = useState('');
 
-    const [listTodo, setlistTodo] = useState([]);
-    const [todoContent, settodoContent] = useState('');
-
-    const listNewTodo = listTodo.filter(item => item.status_id === 1)
-    const listFinishedTodo = listTodo.filter(item => item.status_id === 2)
-    const listCanelledTodo = listTodo.filter(item => item.status_id === 3)
-
-    const handleSaveTodoContent = (event)=>{
-        settodoContent(event.target.value);
+    const handleSaveTodoContent = (event) => {
+        setTodoContent(event.target.value);
     }
 
-    const handleCreaterTodo = (event) =>{
+    const handleCreateTodo = (event) => {
         event.preventDefault();
-        if(!todoContent.trim()){
-            alert('Vui lòng nhập kế hoạch của bạn')
-            return
+        if (!todoContent.trim()) {
+            alert('Vui lòng nhập nội dung công việc');
+            return;
         }
+
+        const newTodo = {
+            id: new Date().getTime(),
+            content: todoContent,
+            status_id: 1,
+            created_at: new Date().getTime(),
+        };
+
+        setListTodo([...listTodo, newTodo]);
+        setTodoContent('');
     }
 
-    const obj = {
-        id: new Date().getTime(),
-        content: todoContent,
-        status_id: 1,
-        created_at: new Date().getTime(),
+    const handleFinish = (todoID) => {
+        const listTodoClone = [...listTodo];
+        listTodoClone.forEach((item)=>{
+            if(item.id === todoID){
+                item.status_id = 2
+            }
+        })
+        setListTodo(listTodoClone);
     }
-    settodoContent('')
-    setlistTodo([...listTodo, obj])
+
+    const handleDelete = (todoID) => {
+        const listTodoClone = [...listTodo];
+        listTodoClone.forEach((item)=>{
+            if(item.id === todoID){
+                item.status_id = 3
+            }
+        })
+        setListTodo(listTodoClone);
+    }
+
+    const handleNew = (todoID) => {
+        const listTodoClone = [...listTodo];
+        listTodoClone.forEach((item)=>{
+            if(item.id === todoID){
+                item.status_id = 1
+            }
+        })
+        setListTodo(listTodoClone);
+    }
+    
+    const listNewTodo = listTodo.filter(item => item.status_id === 1);
+    const listFinishedTodo = listTodo.filter(item => item.status_id === 2);
+    const listCancelledTodo = listTodo.filter(item => item.status_id === 3);
+    
+    const renderListTodo = (listTodoTarget) => {
+        return listTodoTarget.map((item) => (
+            <Todo key={item.id} todo={item} handleFinish={handleFinish} handleDelete={handleDelete} handleNew={handleNew}/>
+            ));
+        }
+        console.log(listTodo);
 
     return (
         <div>
-            <form className="d-flex" role="search" onSubmit={handleCreaterTodo}>
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <form className="d-flex" role="search" onSubmit={handleCreateTodo}>
+                <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Nhập công việc của bạn"
+                    aria-label="Search"
+                    onChange={handleSaveTodoContent}
+                    value={todoContent}
+                />
                 <button className="btn btn-outline-success" type="submit">Lưu</button>
             </form>
-            <div className="d-plex row mt-3 mb-3">
+
+            <div className="d-flex row mt-3 mb-3">
                 <div className="col-4">
                     <h3 className="text-xl bg-primary text-white rounded text-center py-2">Mới</h3>
-                    <Todo/>
+                    {renderListTodo(listNewTodo)}
                 </div>
                 <div className="col-4">
                     <h3 className="text-xl bg-success text-white rounded text-center py-2">Đã hoàn thành</h3>
+                    {renderListTodo(listFinishedTodo)}
                 </div>
                 <div className="col-4">
                     <h3 className="text-xl bg-danger text-white rounded text-center py-2">Đã hủy</h3>
+                    {renderListTodo(listCancelledTodo)}
                 </div>
             </div>
         </div>
